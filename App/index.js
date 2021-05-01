efa0943f39aa815ef2d0e5b0b80c7fb51f94de97
Tsudhishnair <sudhishnair53@gmail.com>
2021-05-01 22:43:56 +0530

    MChanged screen name to get its value from a constant instead of hard-coded value

--- App/index.js
+++ App/index.js
@@ -7,28 +7,42 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/Home';
 import Navbar from './components/Navbar';
 import { MainContextProvider } from './contexts/MainContext';
+import colors from './config/colors';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const screenNames = {
+    Home: 'Home',
+  };
+  
   return (
-    <SafeAreaView style={styles.rootContainer}>
-      <NavigationContainer>
-        <MainContextProvider>
-          <Stack.Navigator
-            initialRouteName="Home"
-            screenOptions={{
-              header: props => <Navbar {...props} />,
-            }}>
-            <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
-        </MainContextProvider>
-      </NavigationContainer>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.topContainer} />
+      <SafeAreaView style={styles.rootContainer}>
+        <NavigationContainer>
+          <MainContextProvider>
+            <Stack.Navigator
+              initialRouteName="Home"
+              screenOptions={{
+                header: props => <Navbar {...props} />,
+              }}>
+              <Stack.Screen name={screenNames.Home} component={Home} />
+            </Stack.Navigator>
+          </MainContextProvider>
+        </NavigationContainer>
+      </SafeAreaView>
+    </>
   );
 }
+
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
+    backgroundColor:'#f1f1f1'
+  },
+  topContainer: {
+    flex: 0,
+    backgroundColor: colors.brandColor,
   },
 });
